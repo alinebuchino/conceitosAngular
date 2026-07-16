45165fbe08b96226a516c897579967abea5a07f0
Aline Buchino <aline.buchino@hotmail.com>
2026-07-16 20:32:53 -0300

    conceitos basicos vistos atraves da aplicacao de lista de compras

---
+++ src/app/lista-compras/lista-compras.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { ItemLista } from './itemLista';
+
+@Component({
+  selector: 'app-lista-compras',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './lista-compras.component.html',
+  styleUrl: './lista-compras.component.scss'
+})
+export class ListaComprasComponent {
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem() {
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
+    this.item = '';
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
+  }
+
+  limparLista() {
+    this.lista = [];
+  }
+}
