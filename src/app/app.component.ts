b5858245a801d62bec9717e0b62983af863d541a
Aline Buchino <aline.buchino@hotmail.com>
2026-07-20 20:52:12 -0300

    definindo rotas e inicio do desenvolvimento do formulario de cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { ListaComprasComponent } from './lista-compras/lista-compras.component';
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbar } from "@angular/material/toolbar";
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaComprasComponent],
+  imports: [RouterModule, RouterLink, MatToolbar, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
