0574de0f0b253877c91f5e6cc8b30ce3360b9e85
Aline Buchino <aline.buchino@hotmail.com>
2026-08-17 22:32:54 -0300

    capturando eventos de rota para definir titulo e subtitulo de cada pagina

--- src/app/lugaresPasseio/template/layout/layout.component.ts
+++ src/app/lugaresPasseio/template/layout/layout.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { LayoutProps } from './layout.props';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterModule],
+  standalone: false,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  props: LayoutProps = {titulo: '', subTitulo: ''};
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter( () => this.activatedRoute.firstChild !== null ),
+        map( () => this.obterPropriedadesLayout() )
+      ).subscribe((props: LayoutProps) => this.props = props )
+  }
+
+  obterPropriedadesLayout(): LayoutProps {
+    let rotaFilha = this.activatedRoute.firstChild;
+
+    while (rotaFilha?.firstChild) {
+      rotaFilha = rotaFilha.firstChild;
+    }
+
+    return rotaFilha?.snapshot.data as LayoutProps;
+  }
 }
--- src/app/lugaresPasseio/template/template.module.ts
+++ src/app/lugaresPasseio/template/template.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CategoriasModule } from '../categorias/categorias.module';
+import { LayoutComponent } from './layout/layout.component';
 import { TemplateRoutingModule } from './template-routing.module';
 
-
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule,
